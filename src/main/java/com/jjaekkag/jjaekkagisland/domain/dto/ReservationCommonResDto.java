73ebdc1847939801c257c52d9f7d9e3a7b954b38
jeongheekim <8055kjh@gmail.com>
2023-12-07 19:50:39 +0900

    컬럼명 수정 및 사용하지 않는 쿼리 삭제

--- src/main/java/com/jjaekkag/jjaekkagisland/domain/dto/ReservationCommonResDto.java
+++ src/main/java/com/jjaekkag/jjaekkagisland/domain/dto/ReservationCommonResDto.java
@@ -1,7 +1,14 @@
 package com.jjaekkag.jjaekkagisland.domain.dto;
 
+import com.jjaekkag.jjaekkagisland.domain.Lesson;
+import com.jjaekkag.jjaekkagisland.domain.Member;
 import com.jjaekkag.jjaekkagisland.domain.Reservation;
+import com.jjaekkag.jjaekkagisland.domain.ReservationStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  * @author jeongheekim
@@ -11,12 +18,30 @@ import lombok.Getter;
 public class ReservationCommonResDto {
 
     private final Long memberSeq;
+    private final String memberName;
     private final Long reservationSeq;
     private final Long lessonSeq;
+    private final String lessonName;
+    private final int participant;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
 
     public ReservationCommonResDto(Reservation reservation) {
-        this.memberSeq = reservation.getMember().getMemberSeq();
+        Member member = reservation.getMember();
+        Lesson lesson = reservation.getLesson();
+        this.memberSeq = member.getMemberSeq();
+        this.memberName = member.getName();
+
         this.reservationSeq = reservation.getReservationSeq();
-        this.lessonSeq = reservation.getLesson().getLessonSeq();
+        this.participant = reservation.getParticipant();
+
+        this.lessonName = lesson.getName();
+        this.lessonSeq = lesson.getLessonSeq();
+        this.startDate = lesson.getStartDate();
+        this.startTime = lesson.getStartTime();
+        this.endDate = lesson.getEndDate();
+        this.endTime = lesson.getEndTime();
     }
 }
